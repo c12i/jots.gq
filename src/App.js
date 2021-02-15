c7f65d177d4a6d407366748c75023e603bba06a2
Collins Muriuki <murerwacollins@gmail.com>
2021-02-15 22:35:10 +0300

    Set up Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 import Pages from './pages'
 import GlobalStyles from './components/GlobalStyles'
 
+const uri = process.env.API_URI
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+})
+
 const App = () => (
-    <React.Fragment>
+    <ApolloProvider client={client}>
         <GlobalStyles />
         <Pages />
-    </React.Fragment>
+    </ApolloProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
