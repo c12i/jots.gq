60ec44e0d3c7945d1411395f2f28bbbe622bb3b8
Collins Muriuki <murerwacollins@gmail.com>
2021-02-16 01:18:27 +0300

    Extract a Note and NoteFeed components

---
+++ src/components/Note.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+import { format } from 'date-fns'
+
+const StyledNote = styled.article`
+    max-width: 800px;
+    margin: 0 auto;
+`
+
+const Metadata = styled.div`
+    @media (min-width: 500px) {
+        display: flex;
+        align-items: top;
+    }
+`
+
+const MetaInfo = styled.div`
+    padding-right: 1em;
+`
+
+const UserActions = styled.div`
+    margin-left: auto;
+`
+
+const Note = ({ note }) => {
+    return (
+        <StyledNote>
+            <Metadata>
+                <MetaInfo>
+                    <img
+                        src={note.author.avatar}
+                        alt={`${note.author.username} avatar`}
+                        height="50px"
+                    />
+                </MetaInfo>
+                <MetaInfo>
+                    <em>by </em>
+                    {note.author.username} <br />{' '}
+                    {format(note.createdAt, 'MMM Do YYYY')}
+                </MetaInfo>
+                <UserActions>
+                    <em>Favorites: </em>
+                    {note.favoriteCount}
+                </UserActions>
+            </Metadata>
+            <ReactMarkdown source={note.content} />
+        </StyledNote>
+    )
+}
+
+export default Note
