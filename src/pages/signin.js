05f7150f3349c8113c5c9846e769660256e1695e
Collins Muriuki <murerwacollins@gmail.com>
2021-02-26 10:24:23 +0300

    Auth mutations (signIn & signUp) call the client side setCurrentUser mutation on complete rather than directly writing to cache

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,24 +1,22 @@
 import React, { useEffect } from 'react'
-import { useApolloClient, useMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client'
 
 import UserForm from '../components/UserForm'
 import Center from '../components/Center'
-import { GET_ME } from '../gql/query'
-import { SIGN_IN } from '../gql/mutation'
+import { SIGN_IN, SET_CURRENT_USER } from '../gql/mutation'
 
 const SignIn = ({ history }) => {
     useEffect(() => {
         document.title = 'Sign In — Jots'
     })
 
-    const client = useApolloClient()
+    const [setCurrentUser] = useMutation(SET_CURRENT_USER)
     const [signIn, { loading, error }] = useMutation(SIGN_IN, {
         onCompleted: data => {
             localStorage.setItem('token', data.signIn.token)
-            client.writeData({ data: { isLoggedIn: true } })
+            setCurrentUser({ variables: { user: data.signIn.user } })
             history.push('/')
-        },
-        refetchQueries: [{ query: GET_ME }]
+        }
     })
 
     return (
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,21 +1,20 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import { useMutation, useApolloClient } from '@apollo/client'
+import { useMutation } from '@apollo/client'
 
 import Center from '../components/Center'
 import UserForm from '../components/UserForm'
-import { SIGN_UP } from '../gql/mutation'
+import { SIGN_UP, SET_CURRENT_USER } from '../gql/mutation'
 
 const SignUp = ({ history }) => {
     useEffect(() => {
         document.title = 'Sign Up - Jots'
     })
 
-    const client = useApolloClient()
+    const [setCurrentUser] = useMutation(SET_CURRENT_USER)
     const [signUp, { loading, error }] = useMutation(SIGN_UP, {
         onCompleted: ({ signUp }) => {
             localStorage.setItem('token', signUp.token)
-            client.writeData({ data: { isLoggedIn: true } })
+            setCurrentUser({ variables: { user: signUp.user } })
             history.push('/')
         },
         onError: err => {
